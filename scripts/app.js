ffa18e6c905056c4b25ba163d16a02e0aa587b7f
qmsparks <q.m.sparks@gmail.com>
2023-07-19 11:36:32 -0600

    appends new elements to dom at point of click

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 const handleClick = e => {
-  console.log(e);
+  const div = document.createElement('div');
+  div.style.left = `${e.x}px`;
+  div.style.top = `${e.y}px`;
+  document.body.append(div);
 }
 
 
