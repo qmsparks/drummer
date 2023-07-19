b55c9824063f15baff9e50e6c5aec79986e29ad9
qmsparks <q.m.sparks@gmail.com>
2023-07-19 13:24:25 -0600

    centers image on point of click

--- scripts/app.js
+++ scripts/app.js
@@ -3,21 +3,23 @@ for (let i=0; i <=43; i++) {
   images.push(`images/drummer${i}.jpeg`)
 }
 
-const handleClick = e => {
-  // const div = document.createElement('div');
-  // document.body.append(div);
-  // div.style.left = `${e.x - (div.scrollWidth/2)}px`;
-  // div.style.top = `${e.y - (div.scrollHeight/2)}px`;
-
+const createImgElement = () => {
   const img = document.createElement('img');
   const randomNumber = Math.floor(Math.random()*images.length);
   const randomImg = images.splice(randomNumber, 1);
-
-  
   img.src = randomImg;
-  img.style.left = `${e.x - (img.width/2)}px`;
+
+  return img;
+}
+
+const handleClick = e => {
+  const img = createImgElement();
+  img.onload = () => {
+    img.style.left = `${e.x - (img.width/2)}px`;
   img.style.top = `${e.y - (img.height/2)}px`;
-  document.body.append(img)
+  }
+
+  document.body.append(img);
 }
 
 
