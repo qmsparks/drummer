464505ff8fd52f4633fdcc29fe8b799adefd2f71
qmsparks <q.m.sparks@gmail.com>
2023-07-19 12:36:08 -0600

    adds images on click instead of empty div

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,23 @@
+const images = [];
+for (let i=0; i <=43; i++) {
+  images.push(`images/drummer${i}.jpeg`)
+}
+
 const handleClick = e => {
-  const div = document.createElement('div');
-  document.body.append(div);
-  div.style.left = `${e.x - (div.scrollWidth/2)}px`;
-  div.style.top = `${e.y - (div.scrollHeight/2)}px`;
+  // const div = document.createElement('div');
+  // document.body.append(div);
+  // div.style.left = `${e.x - (div.scrollWidth/2)}px`;
+  // div.style.top = `${e.y - (div.scrollHeight/2)}px`;
+
+  const img = document.createElement('img');
+  const randomNumber = Math.floor(Math.random()*images.length);
+  const randomImg = images.splice(randomNumber, 1);
+
+  
+  img.src = randomImg;
+  img.style.left = `${e.x - (img.width/2)}px`;
+  img.style.top = `${e.y - (img.height/2)}px`;
+  document.body.append(img)
 }
 
 
